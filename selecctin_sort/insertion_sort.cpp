4b4638cd3454fa97cec23bdc52843ffebb70b359
Princeraj0304 <priinceraj5403@gmail.com>
2025-11-16 23:54:42 +0530

    commit

---
+++ selecctin_sort/insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int insertionsort(int arr[],int size){
+    for (int i = 1; i < size; i++)
+    {
+        int temp=arr[i];
+        int j=i-1;
+
+        for (; j >=0; j--)
+        {
+            if (arr[j]>temp)
+            {
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+            
+        }
+        
+
+    }
+
+}
+
+int main()
+{
+    int arr[]={12,5,66,2,6,8};
+    int size=6;
+    insertionsort(arr,size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
---
+++ selecctin_sort/selection_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+void sorted(int arr[],int size){
+    for (int i = 0; i < size; i++)
+    {
+        int minindex = i;  
+        for (int j = i+1; j < size; j++)
+        {
+            if (arr[minindex]>arr[j])
+            {
+                minindex=j;
+            
+            } 
+        }
+          swap(arr[minindex],arr[i]);
+            
+    }
+}
+
+int main()
+{
+    int arr[]={12,43,23,6,7,44,32};
+    int size=7;
+
+    sorted(arr,size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+ 
+    return 0;
+}
